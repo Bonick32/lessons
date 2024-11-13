7f7d87bf7915c5168ffec3354e36ac46bdd8c6cd
Bonick32 <Bonick3244@gmail.com>
2024-11-13 21:24:33 +0300

    003

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Николай"
+print("Name: " + name)
+age = 33
+print("Age: " + str(age))
+new_age = age + 4
+print("New age: " + str(new_age))
+is_student = True
+print("Is Student: " + str(is_student))
