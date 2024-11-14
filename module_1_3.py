b3be2ece778a27b3dfb4b1b214d982eff37cc103
Bonick32 <Bonick3244@gmail.com>
2024-11-14 21:28:58 +0300

    005

--- module_1_3.py
+++ module_1_3.py
@@ -1,11 +1,11 @@
 name = "Николай"
-print("Name: " + name)
+print("Name:" , name)
 
 age = 33
-print("Age: " + str(age))
+print("Age:" , age)
 
 new_age = age + 4
-print("New age: " + str(new_age))
+print("New age:" , new_age)
 
 is_student = True
-print("Is Student: " + str(is_student))
+print("Is Student:" , is_student)
