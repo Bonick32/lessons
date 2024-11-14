4f196a417062aba67c39b3044e60f8cc95ea6a80
Bonick32 <Bonick3244@gmail.com>
2024-11-14 22:31:02 +0300

    006

--- module_1_4.py
+++ module_1_4.py
@@ -6,10 +6,10 @@ time_per_task = (hours_spent / number_of_task_completed)
 # не разобрался, как короче сделать так, что запятая приклеилась к переменной
 # сначала сделал без (+ ","), но добавлял запятую перед текстом в print(), тогда и перед запятой пробел
 
-print("Курс:", course_name + ",", "всего задач:", str(number_of_task_completed) + ",",\
+print("Курс:", course_name + ",", "всего задач:", str(number_of_task_completed) + ",",
       "затрачено часов:", str(hours_spent) + ",", "Среднее время выполнения:", str(time_per_task) + ".")
 
 # непосредственно первоначальный вариант:
 
-print("Курс:", course_name, ", всего задач:", number_of_task_completed,\
+print("Курс:", course_name, ", всего задач:", number_of_task_completed,
       ", затрачено часов:",hours_spent, ", Среднее время выполнения:", time_per_task)
