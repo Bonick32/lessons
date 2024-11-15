eeb585a0fa4f36b91130d967220c59696cd84683
Bonick32 <Bonick3244@gmail.com>
2024-11-15 21:48:53 +0300

    007

--- module_1_4.py
+++ module_1_4.py
@@ -12,3 +12,6 @@ print("Курс:", course_name + ",", "всего задач:", str(number_of_ta
 
 print(f"Курс: {course_name}, всего задач: {number_of_task_completed}, "
       f"затрачено часов: {hours_spent}, Среднее время выполнения: {time_per_task}.")
+
+print("Курс: " + course_name , "всего задач: " + str(number_of_task_completed) ,
+      "затрачено часов: " + str(hours_spent) , "Среднее время выполнения: " + str(time_per_task) + ".", sep=", ")
