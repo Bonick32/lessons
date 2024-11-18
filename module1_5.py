623925126ae7e2858a34ef0f380a3d5addecf7ef
Bonick32 <Bonick3244@gmail.com>
2024-11-19 01:05:16 +0300

    008

---
+++ module1_5.py
@@ -0,0 +1,12 @@
+immutable_var = 1, 2, True, "Forgot"
+print(immutable_var)
+
+# immutable_var[0] = 22 Данная операция невозможна из-за того,
+#                       что элементы, содержащиеся в кортеже - неизменяемы
+#                       проще говоря, Кортеж - неизменяем
+
+mutable_list = [56, 1, "Text", False, 8, "2 Text"]
+mutable_list.append(6)
+mutable_list[2] = 2
+mutable_list[3] = True
+print(mutable_list)
